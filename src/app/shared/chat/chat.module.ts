44724a68bf3a37011677078f78cf5766b73fe38f
lazarljubenovic <lazar.ljubenovic.1995@gmail.com>
2016-11-24 23:47:42 +0100

    feat: add chat input box

--- src/app/shared/chat/chat.module.ts
+++ src/app/shared/chat/chat.module.ts
@@ -2,11 +2,12 @@ import {NgModule} from "@angular/core";
 import {CommonModule} from "@angular/common";
 import {ChatComponent} from "./chat.component";
 import {ChatMessageComponent} from "./chat-message/chat-message.component";
-import {MarkdownService} from "../markdown.service";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
     imports: [
         CommonModule,
+        FormsModule,
     ],
     declarations: [
         ChatComponent,
