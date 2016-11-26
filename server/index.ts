16a9ad3c0f298fe718101326df487e242cfbbb51
Pritilender <sajn_ap@live.com>
2016-11-26 15:08:13 +0100

    refactor: tabs to spaces

--- server/index.ts
+++ server/index.ts
@@ -10,105 +10,105 @@ const app = express();
 const port = 4000;
 
 const dummyMessages: ChatMessageInfo[] = [
-	{
-		timeStamp: new Date(),
-		senderHandle: `lazarljubenovic`,
-		senderName: `Lazar Ljubenović`,
-		senderHash: `ff8adece0631821959f443c9d956fc39`,
-		message: `Hello World!`,
-	},
-	{
-		timeStamp: new Date(),
-		senderHandle: `pritilender`,
-		senderName: `Mihajlo Ilijić`,
-		senderHash: `ff8adece0631821959f443c9d956fc39`,
-		message: `Hello World! **bold** _italic_ ~~strike~~`,
-	},
-	{
-		timeStamp: new Date(),
-		senderHandle: `lazarljubenovic`,
-		senderName: `Lazar Ljubenović`,
-		senderHash: `ff8adece0631821959f443c9d956fc39`,
-		message: `Hello World! [link](www.google.com)`,
-	},
-	{
-		timeStamp: new Date(),
-		senderHandle: `pritilender`,
-		senderName: `Mihajlo Ilijić`,
-		senderHash: `ff8adece0631821959f443c9d956fc39`,
-		message: `Hello World! :) :* ;) :(`,
-	},
-	{
-		timeStamp: new Date(),
-		senderHandle: `lazarljubenovic`,
-		senderName: `Lazar Ljubenović`,
-		senderHash: `ff8adece0631821959f443c9d956fc39`,
-		message: `Hello World! :joy: :heart: :sob: :+1:`,
-	},
+    {
+        timeStamp: new Date(),
+        senderHandle: `lazarljubenovic`,
+        senderName: `Lazar Ljubenović`,
+        senderHash: `ff8adece0631821959f443c9d956fc39`,
+        message: `Hello World!`,
+    },
+    {
+        timeStamp: new Date(),
+        senderHandle: `pritilender`,
+        senderName: `Mihajlo Ilijić`,
+        senderHash: `ff8adece0631821959f443c9d956fc39`,
+        message: `Hello World! **bold** _italic_ ~~strike~~`,
+    },
+    {
+        timeStamp: new Date(),
+        senderHandle: `lazarljubenovic`,
+        senderName: `Lazar Ljubenović`,
+        senderHash: `ff8adece0631821959f443c9d956fc39`,
+        message: `Hello World! [link](www.google.com)`,
+    },
+    {
+        timeStamp: new Date(),
+        senderHandle: `pritilender`,
+        senderName: `Mihajlo Ilijić`,
+        senderHash: `ff8adece0631821959f443c9d956fc39`,
+        message: `Hello World! :) :* ;) :(`,
+    },
+    {
+        timeStamp: new Date(),
+        senderHandle: `lazarljubenovic`,
+        senderName: `Lazar Ljubenović`,
+        senderHash: `ff8adece0631821959f443c9d956fc39`,
+        message: `Hello World! :joy: :heart: :sob: :+1:`,
+    },
 ];
 
 let messageRooms = {}; //associative array where each member holds an array of websocket clients
 
 app.use(function (req, res) {
-	res.send({msg: "hello"});
+    res.send({msg: "hello"});
 });
 
 wss.on('connection', ws => {
-	//let location = url.parse(ws.upgradeReq.url, true);
+    //let location = url.parse(ws.upgradeReq.url, true);
 
-	ws.on('message', (message: string) => {
-		let messageObj: Message<any> = JSON.parse(message);
+    ws.on('message', (message: string) => {
+        let messageObj: Message<any> = JSON.parse(message);
 
-		if (messageObj.type == 'chat') {
-			var parsedMessage: ChatMessageInfo = messageObj.payload; //todo fix type
-		}
-		console.log(parsedMessage);
+        if (messageObj.type == 'chat') {
+            var parsedMessage: ChatMessageInfo = messageObj.payload; //todo fix type
+        }
+        console.log(parsedMessage);
 
-		// Create room if it doesn't exists
-		if (messageRooms[parsedMessage.senderHash] == undefined) {
-			messageRooms[parsedMessage.senderHash] = [];
-		}
+        // Create room if it doesn't exists
+        if (messageRooms[parsedMessage.senderHash] == undefined) {
+            messageRooms[parsedMessage.senderHash] = [];
+        }
 
 
-		if (parsedMessage.message == 'init') {
-			// Add user if the message is "init"
-			//todo should have special type for init messages
-			messageRooms[parsedMessage.senderHash].push(ws);
-			console.log("New user joined the room", parsedMessage.senderHash);
-		} else {
-			// Broadcast message to other users in the same room
-			wss.clients.filter(client => (messageRooms[parsedMessage.senderHash].indexOf(client) > -1) && client != ws)
-				.forEach(client => client.send(JSON.stringify(messageObj)));
-		}
-	});
+        if (parsedMessage.message == 'init') {
+            // Add user if the message is "init"
+            //todo should have special type for init messages
+            messageRooms[parsedMessage.senderHash].push(ws);
+            console.log("New user joined the room", parsedMessage.senderHash);
+        } else {
+            // Broadcast message to other users in the same room
+            wss.clients.filter(client => (messageRooms[parsedMessage.senderHash].indexOf(client) > -1) && client != ws)
+                .forEach(client => client.send(JSON.stringify(messageObj)));
+        }
+    });
 
-	ws.on('error', (error) => {
-		console.log(error);
-	});
+    ws.on('error', (error) => {
+        console.log(error);
+    });
 
-	ws.on('close', () => {
-		let rooms = Object.keys(messageRooms);
-		rooms.forEach(room => {
-			let ind = messageRooms[room].indexOf(ws);
+    ws.on('close', () => {
+        let rooms = Object.keys(messageRooms);
+        rooms.forEach(room => {
+            let ind = messageRooms[room].indexOf(ws);
 
-			if (ind > -1) {
-				messageRooms[room].splice(ind, 1);
-				console.log("User has left the room", room);
-			}
-		})
-	});
+            if (ind > -1) {
+                messageRooms[room].splice(ind, 1);
+                console.log("User has left the room", room);
+            }
+        })
+    });
 
-	setTimeout(() => {
-		dummyMessages.forEach(dummyMessage => {
-			ws.send(JSON.stringify({
-				payload: dummyMessage,
-				type: 'chat'
-			}));
-		})
-	}, 1000)
+    setTimeout(() => {
+        dummyMessages.forEach(dummyMessage => {
+            ws.send(JSON.stringify({
+                payload: dummyMessage,
+                type: 'chat'
+            }));
+        })
+    }, 1000)
 });
 
 server.on('request', app);
 server.listen(port, () => {
-	console.log('Listening on ' + server.address().port);
+    console.log('Listening on ' + server.address().port);
 });
--- src/app/shared/chat/chat.component.ts
+++ src/app/shared/chat/chat.component.ts
@@ -1,78 +1,78 @@
 import {
-	Component,
-	OnInit,
-	ViewContainerRef,
-	ViewChild,
-	ComponentFactoryResolver,
-	ComponentRef
+    Component,
+    OnInit,
+    ViewContainerRef,
+    ViewChild,
+    ComponentFactoryResolver,
+    ComponentRef
 } from "@angular/core";
 import {ChatMessageInfo, ChatMessageComponent} from "./chat-message/chat-message.component";
 import {ChatService} from "./chat.service";
 import {Observable} from "rxjs";
 
 @Component({
-	selector: 'grf-chat',
-	templateUrl: './chat.component.html',
-	styleUrls: ['./chat.component.scss'],
+    selector: 'grf-chat',
+    templateUrl: './chat.component.html',
+    styleUrls: ['./chat.component.scss'],
 })
 export class ChatComponent implements OnInit {
 
-	@ViewChild('message', {read: ViewContainerRef})
-	private chatMessageRef: any;
+    @ViewChild('message', {read: ViewContainerRef})
+    private chatMessageRef: any;
 
-	private chatMessageFactory;
+    private chatMessageFactory;
 
-	public chatMessages$: Observable<any>;
+    public chatMessages$: Observable<any>;
 
-	public currentTypedMessage: string;
+    public currentTypedMessage: string;
 
-	private createChatMessage(content: ChatMessageInfo): void {
-		let cmp: ComponentRef<ChatMessageComponent> =
-			this.chatMessageRef.createComponent(this.chatMessageFactory);
-		cmp.instance.info = content;
-	}
+    private createChatMessage(content: ChatMessageInfo): void {
+        let cmp: ComponentRef<ChatMessageComponent> =
+            this.chatMessageRef.createComponent(this.chatMessageFactory);
+        cmp.instance.info = content;
+    }
 
-	public onKeyUp(key: string) {
-		if (key == 'Enter') {
-			let chatMessage: ChatMessageInfo = {
-				senderHandle: 'lazar',
-				senderHash: '231230213412',
-				senderName: 'Lazar Ljubenović',
-				timeStamp: new Date(),
-				message: this.currentTypedMessage
-			};
+    public onKeyUp(key: string) {
+        if (key == 'Enter') {
+            let chatMessage: ChatMessageInfo = {
+            senderHandle: 'lazar',
+                senderHash: '231230213412',
+                senderName: 'Lazar Ljubenović',
+                timeStamp: new Date(),
+                message: this.currentTypedMessage
+            };
 
-			this.sendMessage(chatMessage);
+            this.sendMessage(chatMessage);
 
-			this.createChatMessage(chatMessage);
+            this.createChatMessage(chatMessage);
 
-			this.currentTypedMessage = '';
-		}
-	}
+            this.currentTypedMessage = '';
+        }
+    }
 
-	public sendMessage(message: ChatMessageInfo): void {
-		this.chatService.send(message);
-	}
+    public sendMessage(message: ChatMessageInfo): void {
+        this.chatService.send(message);
+    }
 
-	constructor(private chatService: ChatService,
-				private cfr: ComponentFactoryResolver) {
-	}
+    constructor(private chatService: ChatService,
+                private cfr: ComponentFactoryResolver) {
+    }
 
-	ngOnInit() {
-		this.chatMessageFactory = this.cfr.resolveComponentFactory(ChatMessageComponent);
-		this.chatMessages$ = this.chatService.create("ws://localhost:4000");
+    ngOnInit() {
+        this.chatMessageFactory = this.cfr.resolveComponentFactory(ChatMessageComponent);
+        this.chatMessages$ = this.chatService.create("ws://localhost:4000");
 
-		this.chatService.send({
-			message: "init",
-			senderHandle: "lazar",
-			senderHash: "231230213412",
-			senderName: "Lazar Ljubenovic",
-			timeStamp: new Date(),
-		});
+        this.chatService.send({
+            message: "init",
+            senderHandle: "lazar",
+            senderHash: "231230213412",
+            senderName: "Lazar Ljubenovic",
+            timeStamp: new Date(),
+        });
 
-		this.chatMessages$.subscribe((message: ChatMessageInfo) => {
-			this.createChatMessage(message);
-		});
-	}
+        this.chatMessages$.subscribe((message: ChatMessageInfo) => {
+            this.createChatMessage(message);
+        });
+    }
 
 }
