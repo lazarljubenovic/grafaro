f47121b2ed7ab22a64f02cc850c2c4053b70f958
lazarljubenovic <lazar.ljubenovic.1995@gmail.com>
2016-11-28 21:29:41 +0100

    fix(toast): animation

--- src/app/toast/toast.component.ts
+++ src/app/toast/toast.component.ts
@@ -5,7 +5,8 @@ import {
     animate,
     transition,
     trigger,
-    state
+    state,
+    HostBinding
 } from "@angular/core";
 import {Observable} from "rxjs";
 
@@ -17,15 +18,20 @@ import {Observable} from "rxjs";
         trigger('showHide', [
             state('*', style({
                 transform: 'translateY(0)',
+                opacity: '1',
             })),
             state('void', style({
-                transform: 'translateY(100%)',
+                transform: 'translateY(300%)',
+                opacity: '0',
             })),
-            transition('void <=> *', animate('200ms ease')),
+            transition('void <=> *', animate('400ms ease')),
         ])
     ],
 })
 export class ToastComponent implements OnInit {
+
+    @HostBinding('@showHide')
+    public showHide: boolean = true;
 
     public message$: Observable<string>;
 
