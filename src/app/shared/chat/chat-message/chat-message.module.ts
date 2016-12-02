1b59f9f3ed2ca566b2b6869d0c40efb146c62b68
lazarljubenovic <lazar.ljubenovic.1995@gmail.com>
2016-12-02 11:53:55 +0100

    feat(chat): improve design

--- src/app/shared/chat/chat-message/chat-message.module.ts
+++ src/app/shared/chat/chat-message/chat-message.module.ts
@@ -2,11 +2,13 @@ import {CommonModule} from "@angular/common";
 import {FormsModule} from "@angular/forms";
 import {NgModule} from "@angular/core";
 import {ChatMessageComponent} from "./chat-message.component";
+import {JdenticonModule} from "../../../jdenticon/jdenticon.module";
 
 @NgModule({
     imports: [
         CommonModule,
         FormsModule,
+        JdenticonModule,
     ],
     declarations: [
         ChatMessageComponent,
