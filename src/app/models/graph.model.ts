9f25ce0920444ed3b35b52ceb0b1a60b3eceb5b5
Pritilender <sajn_ap@live.com>
2017-01-18 18:21:07 +0100

    fix(algorithm): rename node label

--- src/app/models/graph.model.ts
+++ src/app/models/graph.model.ts
@@ -93,7 +93,8 @@ export class Graph {
     public getNodeId(nodeLabel: NodeLabel): NodeId {
         const node = this._nodes.find(node => node.label == nodeLabel);
         if (node == null) {
-            throw new Error(`Node with label ${nodeLabel} doesn't exist`);
+            // throw new Error(`Node with label ${nodeLabel} doesn't exist`);
+            return null;
         }
         return node.id;
     }
