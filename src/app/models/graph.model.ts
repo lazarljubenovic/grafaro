a4f4c4b20e4aaa023724d494a4e2b5695e1836ac
Pritilender <sajn_ap@live.com>
2017-02-01 21:15:52 +0100

    test(graph): uncross tests

--- src/app/models/graph.model.ts
+++ src/app/models/graph.model.ts
@@ -93,8 +93,7 @@ export class Graph {
     public getNodeId(nodeLabel: NodeLabel): NodeId {
         const node = this._nodes.find(node => node.label == nodeLabel);
         if (node == null) {
-            // throw new Error(`Node with label ${nodeLabel} doesn't exist`);
-            return null;
+            throw new Error(`Node with label ${nodeLabel} doesn't exist`);
         }
         return node.id;
     }
