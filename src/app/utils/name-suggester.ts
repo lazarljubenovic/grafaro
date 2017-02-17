85993a9c0875defe8104cb2bd7ba7f1549bf4dca
Pritilender <sajn_ap@live.com>
2017-02-18 00:11:38 +0100

    fix(util): make name-suggester a bit smarter

--- src/app/utils/name-suggester.ts
+++ src/app/utils/name-suggester.ts
@@ -3,12 +3,20 @@
  */
 export function suggestNextName(arr: string[]): string {
     arr.sort();
+    if (arr.length == 0) {
+        return 'A';
+    }
     const last: string = arr[arr.length - 1];
     if (!Number.isNaN(Number.parseInt(last))) {
-        return last + 1;
+        return (Number.parseInt(last) + 1).toString();
     } else {
-        const lastCharCode: number = last.slice(-1).charCodeAt(0);
-        const newLastChar: string = String.fromCharCode(lastCharCode + 1);
-        return last.slice(0, -1).concat(newLastChar);
+        const lastChar: string = last.slice(-1);
+        if (lastChar == 'z' || lastChar == 'Z') {
+            return last + '1';
+        } else {
+            const lastCharCode: number = lastChar.charCodeAt(0);
+            const newLastChar: string = String.fromCharCode(lastCharCode + 1);
+            return last.slice(0, -1).concat(newLastChar);
+        }
     }
 }
